c2f9f538128d44e0869e8663cf5dfe2a9dc78d58
ChuThuyQuynh1110 <125940627+ChuThuyQuynh1110@users.noreply.github.com>
2024-03-13 23:22:26 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/async_mainn/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.async_mainn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/article_model.dart
@@ -0,0 +1,28 @@
+class Article {
+  final String title;
+  final String urlToImage;
+  final String content;
+  final String source;
+  final String publishedAt;
+  final String category;
+
+  Article({
+    required this.title,
+    required this.urlToImage,
+    required this.content,
+    required this.source,
+    required this.publishedAt,
+    required this.category,
+  });
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+      title: json['title'] ?? '',
+      urlToImage: json['urlToImage'] ?? '',
+      content: json['content'] ?? '',
+      source: json['source']['name'] ?? '',
+      publishedAt: json['publishedAt'] ?? '',
+      category: json['category'] ?? '',  // Thêm lĩnh vực
+    );
+  }
+}
